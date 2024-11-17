ac238563b47e7cb2feb627e56ac2ae898f928ac2
Niccola Winter <niccola.winter@students.codeinstitute.net>
2024-11-17 09:06:42 +0000

    added and test print for calculating shipments & get sales data

--- run.py
+++ run.py
@@ -16,4 +16,33 @@ SHEET = GSPREAD_CLIENT.open('shipping_inventory')
 sales = SHEET.worksheet('sales')
 data = sales.get_all_values()
 
-print(data)
+
+
+# Calculate total shipments per location
+def calculate_shipments(shipment_data):
+    shipments_summary = defaultdict(int)
+    for shipment in shipment_data:
+        shipments_summary[shipment["location"]] += shipment["quantity"]
+    return shipments_summary
+
+def get_sales_data():
+    """
+    Get sales figures input from the user.
+    Run a loop to collect a valid string of data from the terminal, 
+    which must be a string of 10 numbers separated
+    by commas. The loop will repeatedly request data, until it is valid.
+    """
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be 10 numbers, separated by commas.")
+        print("Example: 1,2,3,4,5,7,8,9,10\n")
+
+        data_str = input("Enter your data here:/n")
+
+        sales_data = data_str.split(",")
+
+        if validate_data(sales_data):
+            print("Data is valid!")
+            break
+
+    return sales_data
